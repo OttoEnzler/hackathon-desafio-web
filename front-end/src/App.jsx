f409cdbac56d3cdcb87627ce1ad69aecafa9c969
CinRoci <CinthiaMi2012@hotmail.com>
2024-07-20 14:05:24 -0400

    fron cambios

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
-import CategoryList from './components/CategoryList.jsx';
+import { useEffect, useState } from 'react';
+import InicioPage from './pages/inicio/InicioPage';
 
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<CategoryList />} />
+      <Route path="/" element={<LoginPage />} />
+      <Route path='/inicio' element={<InicioPage/>}/>
     </Routes>
   );
 }
---
+++ front-end/src/pages/inicio/InicioPage.jsx
@@ -0,0 +1,11 @@
+import CategoryList from "../../components/CategoryList";
+
+function InicioPage() {
+    return (
+        <div>
+            <CategoryList />
+        </div>
+    );
+}
+
+export default InicioPage;
